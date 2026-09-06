5279bf3c17c05078f2d5ce18f6aaf6284cb11b12
kyler404 <ikun6985@gmail.com>
2026-09-06 14:36:30 +0800

    feat

---
+++ scripts/shuiyinbao.js
@@ -0,0 +1,17 @@
+let body = $response.body;
+
+body = JSON.parse(body);
+
+if (body.content.waterVipInfo === null) {
+    // 为 null，添加默认值
+    body.content.waterVipInfo = {"typeName": "By过客", "remainDays": 9999};
+} else {
+    // 有值，替换内容
+        body.content.waterVipInfo.typeName = "By过客";
+        body.content.waterVipInfo.remainDays = 2;
+}
+
+body = JSON.stringify(body);
+
+// 如果有 $done 回调
+$done({body});
