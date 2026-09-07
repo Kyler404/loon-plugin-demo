8bf5bb03de1faf28c3b668dd06120adee242160b
Kyler404 <142188170+Kyler404@users.noreply.github.com>
2026-09-07 22:31:14 +0800

    更新 hxq1.js

--- scripts/hxq/hxq1.js
+++ scripts/hxq/hxq1.js
@@ -3,8 +3,8 @@ let body = $response.body;
 
 body = JSON.parse(body);
 
-body.user.nick = "By过客";
 body.vnjoy.as = 1;
+body.vnjoy.at = 1;
 body.vnjoy.grade.level = 3;
 
 body = JSON.stringify(body);
